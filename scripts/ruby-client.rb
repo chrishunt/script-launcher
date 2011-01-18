955868e89edc479a5897ee01e0a241dc19c15810
Chris Hunt <huntca@gmail.com>
2011-01-18 15:27:01 -0500

    Added rescue code to client when server is not available.

--- scripts/ruby-client.rb
+++ scripts/ruby-client.rb
@@ -3,7 +3,11 @@ require 'socket'
 hostname = 'localhost'
 port = '3010'
 
-socket = TCPSocket.open(hostname, port)
-socket.puts "hellolocalhost"
-socket.puts "/Users/huntca/Dropbox/src/progeny/win-script-launcher/test.sh"
-puts socket.gets
+begin
+  socket = TCPSocket.open(hostname, port)
+  socket.puts "hellolocalhost"
+  socket.puts "/Users/huntca/Dropbox/src/progeny/win-script-launcher/test.sh"
+  puts socket.gets
+rescue
+  puts "Error connecting to server."
+end
